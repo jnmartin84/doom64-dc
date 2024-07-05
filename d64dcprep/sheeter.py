a8a96ce4bdb818d32f6c8ef807a5928f1482042c
jnmartin84 <jnmartin84@gmail.com>
2024-07-05 08:12:00 -0400

    update tool that builds non enemy texture

--- d64dcprep/sheeter.py
+++ d64dcprep/sheeter.py
@@ -1,25 +1,17 @@
 from PIL import Image
 import json
 
-filenames = ["json/spectre.json", "json/barniteshot.json", "json/fattrect3.json", "json/playtrooposs.json", "json/skulbosshead.json", "json/fattrect1.json", "json/non_enemy.json", "json/remcybr.json", "json/fattrect2.json", "json/painbsp.json", "json/sargfirstcybr.json"]
+filenames = ["json/non_enemy.json"]
 for filename in filenames:
-#    print(filename[5:-5])
     result = Image.new('RGB', (1024,1024), color=(255,0,255,255))
     with open(filename, "r") as read_file:
         data = json.load(read_file)
         for sprite in data['sprites']:
-            if "non_enemy" in filename:
-                fname = "nonenemy/" + sprite['fileName'].upper().replace("PNG","png")
-            else:
-                fname = "monster/" + sprite['fileName'].upper().replace("PNG","png")
-#            print(fname)
+            fname = "nonenemy/" + sprite['fileName'].upper().replace("PNG","png")
             x = int(sprite['x'])
             y = int(sprite['y'])
             width = float(sprite['width'])
             height = float(sprite['height'])
             image_n = Image.open(fname)
             result.paste(im=image_n, box=(x,y))
-    if "non_enemy" in filename:
-        result.save("sheets/" + filename[5:-5] + ".png")
-    else:
-        result.save("sheets/" + filename[5:-5] + ".png")   
+    result.save("sheets/" + filename[5:-5] + ".png")
