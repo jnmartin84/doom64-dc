1b26d4cce05253850ec68d33a9b8a0bc76e9cb8a
jnmartin84 <jnmartin84@gmail.com>
2024-07-07 22:02:41 -0400

    Update build.sh

--- wadtool/build.sh
+++ wadtool/build.sh
@@ -2,4 +2,4 @@
 gcc -O3 -c encode.c -o encode.o
 gcc -O3 -c imgproc.c -o imgproc.o
 gcc -O3 -c wadtool.c -o wadtool.o
-gcc encode.o imgproc.o wadtool.o -o wadtool.exe
+gcc encode.o imgproc.o wadtool.o -o wadtool
